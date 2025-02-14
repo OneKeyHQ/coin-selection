24bc2582098946e5a7f2199ded17785299bfa2dc
ByteZhang <ByteZhang@protonmail.com>
2025-02-14 17:43:06 +0800

    feat: add hasSetTagWithBody method (#7)

--- src/utils/onekey/index.ts
+++ src/utils/onekey/index.ts
@@ -2,12 +2,14 @@ import { composeTxPlan } from './transaction';
 import { signTransaction, signTx } from './signTx';
 import { dAppUtils } from './dapp';
 import { txToOneKey } from './txToOneKey';
+import { hasSetTagWithBody } from './hasSetTag';
 
 const onekeyUtils = {
   composeTxPlan,
   signTransaction,
   signTx,
   txToOneKey,
+  hasSetTagWithBody,
 };
 
 export { onekeyUtils, dAppUtils };
